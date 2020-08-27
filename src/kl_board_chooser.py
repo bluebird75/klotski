90121923a3d57dce0a8d36e66b611ddb0e6cce19
Philippe F <phil.fremy@free.fr>
2020-08-27 07:58:34 +0200

    Indent using spaces

--- src/kl_board_chooser.py
+++ src/kl_board_chooser.py
@@ -13,50 +13,50 @@ from kl_map import KLMap
 
 class KlMinimapProvider(QAbstractListModel):
 
-	def __init__(self, level_dict: Dict[int, KLMap], mini_maps_dict: Dict[int, QPixmap ]) -> None:
-		super().__init__()
-		assert len(mini_maps_dict) > 0, "minimap board dict is empty!"
-		assert len(level_dict) > 0, "level dict is empty!"
+    def __init__(self, level_dict: Dict[int, KLMap], mini_maps_dict: Dict[int, QPixmap ]) -> None:
+        super().__init__()
+        assert len(mini_maps_dict) > 0, "minimap board dict is empty!"
+        assert len(level_dict) > 0, "level dict is empty!"
 
-		self.level_dict = level_dict
-		self.mini_maps_dict = mini_maps_dict
+        self.level_dict = level_dict
+        self.mini_maps_dict = mini_maps_dict
 
-	def rowCount(self, parentIdx: QModelIndex) -> int:
-		# -1 because we don't want to display splash screen as a board
-		return len(self.level_dict) - 1
+    def rowCount(self, parentIdx: QModelIndex) -> int:
+        # -1 because we don't want to display splash screen as a board
+        return len(self.level_dict) - 1
 
-	def data(self, index: QModelIndex, role: int = Qt.DisplayRole) -> Union[str, QPixmap, QVariant]:
-		if not index.isValid():
-			return QVariant()
+    def data(self, index: QModelIndex, role: int = Qt.DisplayRole) -> Union[str, QPixmap, QVariant]:
+        if not index.isValid():
+            return QVariant()
 
-		row = index.row()
-		if role == Qt.DisplayRole:
-			return self.level_dict[row+1].name
+        row = index.row()
+        if role == Qt.DisplayRole:
+            return self.level_dict[row+1].name
 
-		if role == Qt.DecorationRole:
-			return self.mini_maps_dict[row+1]
+        if role == Qt.DecorationRole:
+            return self.mini_maps_dict[row+1]
 
-		return QVariant()
+        return QVariant()
 
 
 class KLBoardChooser(QDialog) :
 
-	def __init__(self, minimapProvider: KlMinimapProvider, parent: QWidget) -> None:
-		super().__init__(parent)
-		ly = QVBoxLayout( self )
-		self.iv = QListView( self )
-		self.iv.setModel( minimapProvider )
-		ly.addWidget( self.iv, 1 )
-		self.iv.setViewMode( QListView.IconMode )
-		self.iv.setWrapping(True)
-		self.iv.setResizeMode( QListView.Adjust )
+    def __init__(self, minimapProvider: KlMinimapProvider, parent: QWidget) -> None:
+        super().__init__(parent)
+        ly = QVBoxLayout( self )
+        self.iv = QListView( self )
+        self.iv.setModel( minimapProvider )
+        ly.addWidget( self.iv, 1 )
+        self.iv.setViewMode( QListView.IconMode )
+        self.iv.setWrapping(True)
+        self.iv.setResizeMode( QListView.Adjust )
 
-		self.iv.activated.connect(self.mini_map_selected)
-		self.resize( 500, 500  )
+        self.iv.activated.connect(self.mini_map_selected)
+        self.resize( 500, 500  )
 
 
-	def mini_map_selected(self, item: QModelIndex) -> None:
-		if not item: return
-		self.done(item.row() + 1)
+    def mini_map_selected(self, item: QModelIndex) -> None:
+        if not item: return
+        self.done(item.row() + 1)
 
 
--- src/kl_enum.py
+++ src/kl_enum.py
@@ -38,74 +38,74 @@ For more information about PyQt, see https://www.riverbankcomputing.com/software
 
 # Piece is used in the map structure
 class Piece:
-	wall 	= '#'
-	heart 	= '*'
-	pieces 	= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	s_wall 	= '-'
-	goal 	= '.'
-	border 	= '@'
-	space 	= ' '
-	none    = 'none'
+    wall    = '#'
+    heart   = '*'
+    pieces  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    s_wall  = '-'
+    goal    = '.'
+    border  = '@'
+    space   = ' '
+    none    = 'none'
 
 def is_piece(p: str) -> bool:
-	'''Analyse the graphic element described by this string and return True if this is a moveable block'''
-	for i in p:
-		if not i in Piece.pieces:
-			return False
-	return True
+    '''Analyse the graphic element described by this string and return True if this is a moveable block'''
+    for i in p:
+        if not i in Piece.pieces:
+            return False
+    return True
 
 
-# Tile is use for drawing only	
+# Tile is use for drawing only  
 class Tile:
-	corner 	 	= 0
-	hor_edge 	= 1
-	ver_edge 	= 2
-	miss_corner = 3
-	inner 		= 4
+    corner      = 0
+    hor_edge    = 1
+    ver_edge    = 2
+    miss_corner = 3
+    inner       = 4
 
-	piece		= 0
-	m_piece		= 1
-	wall		= 2
-	s_wall		= 3
-	goal 		= 4
+    piece       = 0
+    m_piece     = 1
+    wall        = 2
+    s_wall      = 3
+    goal        = 4
 
-	space		= 5 # used only for mini tiles color
+    space       = 5 # used only for mini tiles color
 
 
 mini_tile_colors = {
-	Tile.piece   : Qt.yellow,
-	Tile.m_piece : Qt.red,
-	Tile.wall    : Qt.gray,
-	Tile.s_wall  : Qt.lightGray,
-	Tile.goal    : Qt.darkRed,
-	Tile.space   : Qt.black
-}	# type: Dict[int, Qt.GlobalColor]
+    Tile.piece   : Qt.yellow,
+    Tile.m_piece : Qt.red,
+    Tile.wall    : Qt.gray,
+    Tile.s_wall  : Qt.lightGray,
+    Tile.goal    : Qt.darkRed,
+    Tile.space   : Qt.black
+}   # type: Dict[int, Qt.GlobalColor]
 
 pix_list = [
-	"corner",
-	"hor_edge",
-	"ver_edge",
-	"miss_corner",
-	"inner"
+    "corner",
+    "hor_edge",
+    "ver_edge",
+    "miss_corner",
+    "inner"
 ] # type: List[str]
 
 
 tile_list = {
-	"piece"   : Tile.piece,
-	"m_piece" : Tile.m_piece,
-	"wall"    : Tile.wall ,
-	"s_wall"  : Tile.s_wall,
-	"goal"    : Tile.goal
+    "piece"   : Tile.piece,
+    "m_piece" : Tile.m_piece,
+    "wall"    : Tile.wall ,
+    "s_wall"  : Tile.s_wall,
+    "goal"    : Tile.goal
 } # type: Dict[str, int]
 
 
 # Translation between map content and tiles
 piece_tile = {
-	Piece.heart      : Tile.m_piece,
-	Piece.pieces[0] : Tile.piece,
-	Piece.wall      : Tile.wall,
-	Piece.s_wall    : Tile.s_wall,
-	Piece.goal      : Tile.goal,
-	Piece.space     : Tile.space
+    Piece.heart      : Tile.m_piece,
+    Piece.pieces[0] : Tile.piece,
+    Piece.wall      : Tile.wall,
+    Piece.s_wall    : Tile.s_wall,
+    Piece.goal      : Tile.goal,
+    Piece.space     : Tile.space
 } # type: Dict[str, int]
 
