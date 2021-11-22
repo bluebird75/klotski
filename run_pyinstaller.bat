517dd6993e13dbc714d6a8e8ee2c0ca71dc6e6e1
Philippe FREMY <philippe.fremy@idemia.com>
2021-11-22 22:25:46 +0100

    add other options. Icon does not work btw

--- run_pyinstaller.bat
+++ run_pyinstaller.bat
@@ -3,4 +3,5 @@ pyinstaller run_klotski.py ^
 	--add-data src\boards.kts;. ^
 	--add-binary src\klotski-icon.png;. ^
 	--add-binary src\klotski-tiles.png;. ^
+	--onefile --windowed --icon src\klotski-icon.ico
 	
